fc07fd2751ca8196a828e78e069562e0bf9b9481
Lukas Rieger (Blue) <github@bluecolored.de>
2024-02-29 21:54:38 +0100

    Add NBTWriter

---
+++ src/main/java/de/bluecolored/bluenbt/NBTWriter.java
@@ -0,0 +1,233 @@
+/*
+ * This file is part of BlueNBT, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) Blue (Lukas Rieger) <https://bluecolored.de>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package de.bluecolored.bluenbt;
+
+import com.google.gson.stream.JsonWriter;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class NBTWriter implements Closeable {
+
+    private final DataOutputStream out;
+
+    private int stackPosition = 0;
+    private TagType[] stack = new TagType[32];
+    @Nullable private String nextName = null;
+
+    public NBTWriter(@NotNull OutputStream out) {
+        Objects.requireNonNull(out);
+        if (out instanceof DataOutputStream)
+            this.out = (DataOutputStream) out;
+        else
+            this.out = new DataOutputStream(out);
+
+        JsonWriter
+    }
+
+    @Contract("_ -> this")
+    public NBTWriter name(String name) {
+        if (this.nextName != null)
+            throw new IllegalStateException("The name was already set to '" + name + "'");
+
+        this.nextName = name;
+        return this;
+    }
+
+    public void beginCompound() throws IOException {
+        tag(TagType.COMPOUND);
+        advanceStack();
+    }
+
+    public void beginList(TagType type, int length) throws IOException {
+        tag(TagType.LIST);
+        advanceStack();
+        tag(type);
+        out.writeInt(length);
+    }
+
+    public void endCompound() throws IOException {
+        if (!inCompound()) throw new IllegalStateException("Not in a compound!");
+        reduceStack();
+        tag(TagType.END);
+        afterValue();
+    }
+
+    public void endList() throws IOException {
+        if (!inList()) throw new IllegalStateException("Not in a list!");
+        reduceStack();
+        afterValue();
+    }
+
+    public void value(byte value) throws IOException {
+        tag(TagType.BYTE);
+        out.writeByte(value);
+        afterValue();
+    }
+
+    public void value(short value) throws IOException {
+        tag(TagType.SHORT);
+        out.writeShort(value);
+        afterValue();
+    }
+
+    public void value(int value) throws IOException {
+        tag(TagType.INT);
+        out.writeInt(value);
+        afterValue();
+    }
+
+    public void value(long value) throws IOException {
+        tag(TagType.LONG);
+        out.writeLong(value);
+        afterValue();
+    }
+
+    public void value(float value) throws IOException {
+        tag(TagType.FLOAT);
+        out.writeFloat(value);
+        afterValue();
+    }
+
+    public void value(double value) throws IOException {
+        tag(TagType.DOUBLE);
+        out.writeDouble(value);
+        afterValue();
+    }
+
+    public void value(String value) throws IOException {
+        tag(TagType.STRING);
+        out.writeUTF(value);
+        afterValue();
+    }
+
+    public void value(byte[] value) throws IOException {
+        value(value, 0, value.length);
+    }
+
+    public void value(byte[] value, int off, int len) throws IOException {
+        tag(TagType.BYTE_ARRAY);
+        out.writeInt(value.length);
+        out.write(value, off, len);
+        afterValue();
+    }
+
+    public void value(int[] value) throws IOException {
+        value(value, 0, value.length);
+    }
+
+    public void value(int[] value, int off, int len) throws IOException {
+        tag(TagType.INT_ARRAY);
+        out.writeInt(value.length);
+        int lim = off + len;
+        for (int i = off; i < lim; i++)
+            out.writeInt(value[i]);
+        afterValue();
+    }
+
+    public void value(long[] value) throws IOException {
+        value(value, 0, value.length);
+    }
+
+    public void value(long[] value, int off, int len) throws IOException {
+        tag(TagType.LONG_ARRAY);
+        out.writeInt(value.length);
+        int lim = off + len;
+        for (int i = off; i < lim; i++)
+            out.writeLong(value[i]);
+        afterValue();
+    }
+
+    public boolean inCompound() {
+        return stackPosition > 0 && stack[stackPosition - 1] == TagType.COMPOUND;
+    }
+
+    public boolean inList() {
+        return stackPosition > 0 && stack[stackPosition - 1] == TagType.LIST;
+    }
+
+    private void tag(TagType tag) throws IOException {
+        if (tag != TagType.END && stack[stackPosition] != null) {
+            if (stack[stackPosition] == tag) {
+                if (this.nextName != null)
+                    throw new IllegalStateException("There is a name set. You can't use name() when writing a value inside a list or before end()!");
+                return;
+            }
+            throw new IllegalStateException("Wrong tag-type. Expected type " + stack[stackPosition] + " but got " + tag);
+        }
+        stack[stackPosition] = tag;
+
+        out.write(tag.getId());
+
+        if (tag != TagType.END && !inList()) {
+            if (this.nextName == null) {
+                if (stackPosition > 0)
+                    throw new IllegalStateException("Name is not set. Call name() before writing a value when not inside a list!");
+                this.nextName = ""; // default name to empty string if at root-level
+            }
+
+            out.writeUTF(this.nextName);
+            this.nextName = null;
+        } else if (this.nextName != null) {
+            throw new IllegalStateException("There is a name set. You can't use name() when writing a value inside a list or before end()!");
+        }
+    }
+
+    private void afterValue() {
+        if (!inList())
+            stack[stackPosition] = null;
+    }
+
+    private void advanceStack() {
+        stackPosition++;
+
+        if (stackPosition >= stack.length) {
+            int newLength = stack.length * 2;
+            stack = Arrays.copyOf(stack, newLength);
+        }
+
+        stack[stackPosition] = null;
+    }
+
+    private void reduceStack() {
+        if (stackPosition == 0)
+            throw new IllegalStateException("Can not reduce empty stack!");
+
+        stackPosition--;
+    }
+
+    @Override
+    public void close() throws IOException {
+        out.close();
+
+        if (stackPosition > 0)
+            throw new IOException("Incomplete document!");
+    }
+
+}
---
+++ src/test/java/de/bluecolored/bluenbt/NBTWriterTest.java
@@ -0,0 +1,150 @@
+/*
+ * This file is part of BlueNBT, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) Blue (Lukas Rieger) <https://bluecolored.de>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package de.bluecolored.bluenbt;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.util.zip.GZIPInputStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class NBTWriterTest {
+
+    @Test
+    public void testNbtWriter() throws IOException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        NBTWriter writer = new NBTWriter(out);
+
+        assertFalse(writer.inCompound());
+        assertFalse(writer.inList());
+
+        writer.beginCompound();
+
+        assertTrue(writer.inCompound());
+        assertFalse(writer.inList());
+
+        writer.name("testByte").value((byte) 10);
+        writer.name("testShort").value((short) -23);
+        writer.name("testInt").value(1034);
+        writer.name("testLong").value(289374678734L);
+        writer.name("testFloat").value(-2.653f);
+        writer.name("testDouble").value(4.653d);
+        writer.name("testCompound").beginCompound();
+
+        assertTrue(writer.inCompound());
+        assertFalse(writer.inList());
+
+        writer.name("testList").beginList(TagType.DOUBLE, 3);
+
+        assertTrue(writer.inList());
+        assertFalse(writer.inCompound());
+
+        writer.value(0.43d);
+        writer.value(-0.43d);
+        writer.value(1d);
+        writer.endList(); // testList
+
+        writer.name("testByteArray").value(new byte[]{0, 110, 30, 20, 3, -4});
+        writer.name("testIntArray").value(new int[]{0, -10342, 30, 20, 3, -4});
+        writer.name("testLongArray").value(new long[]{0, 110, 289374678734L, 20, 3, -4});
+
+        assertTrue(writer.inCompound());
+        assertFalse(writer.inList());
+
+        writer.endCompound(); // testCompound
+        writer.endCompound(); // root
+
+        assertFalse(writer.inCompound());
+        assertFalse(writer.inList());
+
+        byte[] data = out.toByteArray();
+
+        ByteArrayInputStream in = new ByteArrayInputStream(data);
+        NBTReader reader = new NBTReader(in);
+
+        reader.beginCompound();
+
+        assertEquals(TagType.BYTE, reader.peek());
+        assertEquals("testByte", reader.name());
+        assertEquals(10, reader.nextByte());
+
+        assertEquals(TagType.SHORT, reader.peek());
+        assertEquals("testShort", reader.name());
+        assertEquals(-23, reader.nextShort());
+
+        assertEquals(TagType.INT, reader.peek());
+        assertEquals("testInt", reader.name());
+        assertEquals(1034, reader.nextInt());
+
+        assertEquals(TagType.LONG, reader.peek());
+        assertEquals("testLong", reader.name());
+        assertEquals(289374678734L, reader.nextLong());
+
+        assertEquals(TagType.FLOAT, reader.peek());
+        assertEquals("testFloat", reader.name());
+        assertEquals(-2.653f, reader.nextFloat());
+
+        assertEquals(TagType.DOUBLE, reader.peek());
+        assertEquals("testDouble", reader.name());
+        assertEquals(4.653d, reader.nextDouble());
+
+        assertEquals(TagType.COMPOUND, reader.peek());
+        assertEquals("testCompound", reader.name());
+        reader.beginCompound();
+
+        assertEquals(TagType.LIST, reader.peek());
+        assertEquals("testList", reader.name());
+        assertEquals(3, reader.beginList());
+
+        assertEquals(TagType.DOUBLE, reader.peek());
+        assertEquals(0.43d, reader.nextDouble());
+        assertEquals(-0.43d, reader.nextDouble());
+        assertEquals(1d, reader.nextDouble());
+
+        assertEquals(TagType.END, reader.peek());
+        reader.endList();
+
+        assertEquals(TagType.BYTE_ARRAY, reader.peek());
+        assertEquals("testByteArray", reader.name());
+        assertArrayEquals(new byte[]{0, 110, 30, 20, 3, -4}, reader.nextByteArray());
+
+        assertEquals(TagType.INT_ARRAY, reader.peek());
+        assertEquals("testIntArray", reader.name());
+        assertArrayEquals(new int[]{0, -10342, 30, 20, 3, -4}, reader.nextIntArray());
+
+        assertEquals(TagType.LONG_ARRAY, reader.peek());
+        assertEquals("testLongArray", reader.name());
+        assertArrayEquals(new long[]{0, 110, 289374678734L, 20, 3, -4}, reader.nextLongArray());
+
+        assertEquals(TagType.END, reader.peek());
+        reader.endCompound();
+
+        assertEquals(TagType.END, reader.peek());
+        reader.endCompound();
+
+    }
+
+}
